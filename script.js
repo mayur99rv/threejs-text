033c3ee0d7fbd54a324dcae38336cbf4f7865f4c
mayur99rv <66148617+mayur99rv@users.noreply.github.com>
2021-03-30 22:46:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // import {OrbitControls} from './OrbitControls.js'
 // import * as THREE from './three.module.js';
-import * as THREE from './three.module.js';
-import {OrbitControls} from './OrbitControls.js'
+
+import * as THREE from './three/build/three.module.js';
+import {OrbitControls} from './three/examples/jsm/controls/OrbitControls.js'
 
 const canvas = document.querySelector('canvas.webgl')
 
